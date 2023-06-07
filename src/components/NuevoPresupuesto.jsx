533340449b8d5a61de7913996a46f1c15f2104e6
Matias Gabriel Riera <mriera@megared.int>
2023-06-07 17:46:22 -0300

    Mensaje del commit

---
+++ src/components/NuevoPresupuesto.jsx
@@ -0,0 +1,33 @@
+import {useState} from 'react'
+import Mensaje from './Mensaje'
+const NuevoPresupuesto = ({presupuesto, setPresupuesto, setIsValidPresupuesto}) => {
+
+  const [mensaje, setMensaje] = useState('')
+
+  const handlePresupuesto = (e) => {
+    e.preventDefault()
+    if(!presupuesto || presupuesto< 0){
+      setMensaje('No es un presupuesto válido')
+      return
+    }
+    setMensaje('')
+    setIsValidPresupuesto(true)
+  }
+  return (
+    <div className='contenedor-presupuesto contenedor sombra'>
+        <form onSubmit={handlePresupuesto} className='formulario'>
+        <div className='campo'>
+            <label htmlFor="">Definir Presupuesto</label>
+            <input type="number" className='nuevo-presupuesto'
+            placeholder='Ingrese su presupuesto'
+            value={presupuesto}
+            onChange={(e)=> setPresupuesto(Number(e.target.value))} />
+        </div>
+        <input type="submit" value="Ingresar" />
+        {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}
+        </form>
+    </div>
+  )
+}
+
+export default NuevoPresupuesto
