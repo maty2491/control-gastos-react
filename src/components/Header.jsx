c18b281ed08475acfcc47600bd508951e87be2e8
Matias Gabriel Riera <mriera@megared.int>
2023-06-10 12:33:13 -0300

    Segunda actualizacion

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,14 @@
 import NuevoPresupuesto from './NuevoPresupuesto'
 import ControlPresupuesto from './ControlPresupuesto'
 
-const Header = ({presupuesto, setPresupuesto,isValidPresupuesto, setIsValidPresupuesto}) => {
+const Header = ({gastos, presupuesto, setPresupuesto,isValidPresupuesto, setIsValidPresupuesto}) => {
   return (
     <header>
         <h1>Planificador de Gastos</h1>
 
         {isValidPresupuesto ? (
             <ControlPresupuesto
+                gastos={gastos}
                 presupuesto={presupuesto}
             />
         ) :(
